311ca8e352936c6625436e834fe7d19a64cf42f9
Slava Zapolschi <zapolski.slava@gmail.com>
2023-04-30 17:59:53 +0300

    Initial commit

---
+++ backend/src/main/java/com/example/fullstackamigoscode2/FullStackAmigoscode2Application.java
@@ -0,0 +1,13 @@
+package com.example.fullstackamigoscode2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAmigoscode2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackAmigoscode2Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/fullstackamigoscode2/FullStackAmigoscode2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackamigoscode2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackAmigoscode2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
